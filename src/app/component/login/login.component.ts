a55c026a4d4be166268a70ed5f5a84aded1e614d
Gaurav <gauravojha1111@gmail.com>
2024-08-03 21:43:27 +0530

    CodeStoreHubLogin

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+email = new FormControl("",[Validators.required,Validators.email]);
+password = new FormControl("",[Validators.required,Validators.minLength(6)]);
+
+loginform = new FormGroup({
+  email:this.email,
+  password:this.password
+})
+
+loginForm(){
+  console.log(this.loginform);
+}
+
+resetForm(){
+  this.loginform.reset();
+  console.log(this.loginform);
+}
 
 }
